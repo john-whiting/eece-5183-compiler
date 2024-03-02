ca2e7888cd819467b28e1846ed682e8709f4b79f
John Whiting <thejwhiting@gmail.com>
2024-03-02 17:30:33 -0500

    feat: added tests for arithmetic node code generation

--- src/generator/expression/arithmetic.rs
+++ src/generator/expression/arithmetic.rs
@@ -89,3 +89,265 @@ impl<'a> CodeGenerator<'a> for ArithmeticNode {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use inkwell::context::Context;
+
+    use crate::parser::{
+        expression::{FactorNode, RelationNode, TermNode},
+        general::NumberNode,
+    };
+
+    use super::*;
+
+    #[test]
+    fn arithmetic_node_generation() {
+        let outside_context = Context::create();
+        let context = CodeGeneratorContext::new(&outside_context);
+        let function_type = context.context.void_type().fn_type(&[], false);
+        let function_value = context.module.add_function("main", function_type, None);
+        let function_entry_block = context.context.append_basic_block(function_value, "entry");
+        context.builder.position_at_end(function_entry_block);
+
+        let tests = vec![
+            (
+                ArithmeticNode::Start(
+                    RelationNode::Start(
+                        TermNode::Start(
+                            FactorNode::Number(NumberNode::IntegerLiteral(3)),
+                            Box::new(TermNode::Nop),
+                        ),
+                        Box::new(RelationNode::Nop),
+                    ),
+                    Box::new(ArithmeticNode::Add(
+                        RelationNode::Start(
+                            TermNode::Start(
+                                FactorNode::Number(NumberNode::IntegerLiteral(3)),
+                                Box::new(TermNode::Nop),
+                            ),
+                            Box::new(RelationNode::Nop),
+                        ),
+                        Box::new(ArithmeticNode::Nop),
+                    )),
+                ),
+                context
+                    .context
+                    .i64_type()
+                    .const_int(6, true)
+                    .as_basic_value_enum(),
+            ),
+            (
+                ArithmeticNode::Start(
+                    RelationNode::Start(
+                        TermNode::Start(
+                            FactorNode::Number(NumberNode::IntegerLiteral(3)),
+                            Box::new(TermNode::Nop),
+                        ),
+                        Box::new(RelationNode::Nop),
+                    ),
+                    Box::new(ArithmeticNode::Add(
+                        RelationNode::Start(
+                            TermNode::Start(
+                                FactorNode::Number(NumberNode::IntegerLiteral(-3)),
+                                Box::new(TermNode::Nop),
+                            ),
+                            Box::new(RelationNode::Nop),
+                        ),
+                        Box::new(ArithmeticNode::Nop),
+                    )),
+                ),
+                context
+                    .context
+                    .i64_type()
+                    .const_int(0, true)
+                    .as_basic_value_enum(),
+            ),
+            (
+                ArithmeticNode::Start(
+                    RelationNode::Start(
+                        TermNode::Start(
+                            FactorNode::Number(NumberNode::IntegerLiteral(3)),
+                            Box::new(TermNode::Nop),
+                        ),
+                        Box::new(RelationNode::Nop),
+                    ),
+                    Box::new(ArithmeticNode::Add(
+                        RelationNode::Start(
+                            TermNode::Start(
+                                FactorNode::Number(NumberNode::FloatLiteral(3.0)),
+                                Box::new(TermNode::Nop),
+                            ),
+                            Box::new(RelationNode::Nop),
+                        ),
+                        Box::new(ArithmeticNode::Nop),
+                    )),
+                ),
+                context
+                    .context
+                    .f64_type()
+                    .const_float(6.0)
+                    .as_basic_value_enum(),
+            ),
+            (
+                ArithmeticNode::Start(
+                    RelationNode::Start(
+                        TermNode::Start(
+                            FactorNode::Number(NumberNode::FloatLiteral(3.0)),
+                            Box::new(TermNode::Nop),
+                        ),
+                        Box::new(RelationNode::Nop),
+                    ),
+                    Box::new(ArithmeticNode::Add(
+                        RelationNode::Start(
+                            TermNode::Start(
+                                FactorNode::Number(NumberNode::FloatLiteral(3.0)),
+                                Box::new(TermNode::Nop),
+                            ),
+                            Box::new(RelationNode::Nop),
+                        ),
+                        Box::new(ArithmeticNode::Nop),
+                    )),
+                ),
+                context
+                    .context
+                    .f64_type()
+                    .const_float(6.0)
+                    .as_basic_value_enum(),
+            ),
+            (
+                ArithmeticNode::Start(
+                    RelationNode::Start(
+                        TermNode::Start(
+                            FactorNode::Number(NumberNode::IntegerLiteral(3)),
+                            Box::new(TermNode::Nop),
+                        ),
+                        Box::new(RelationNode::Nop),
+                    ),
+                    Box::new(ArithmeticNode::Sub(
+                        RelationNode::Start(
+                            TermNode::Start(
+                                FactorNode::Number(NumberNode::IntegerLiteral(3)),
+                                Box::new(TermNode::Nop),
+                            ),
+                            Box::new(RelationNode::Nop),
+                        ),
+                        Box::new(ArithmeticNode::Nop),
+                    )),
+                ),
+                context
+                    .context
+                    .i64_type()
+                    .const_int(0, true)
+                    .as_basic_value_enum(),
+            ),
+            (
+                ArithmeticNode::Start(
+                    RelationNode::Start(
+                        TermNode::Start(
+                            FactorNode::Number(NumberNode::IntegerLiteral(3)),
+                            Box::new(TermNode::Nop),
+                        ),
+                        Box::new(RelationNode::Nop),
+                    ),
+                    Box::new(ArithmeticNode::Sub(
+                        RelationNode::Start(
+                            TermNode::Start(
+                                FactorNode::Number(NumberNode::IntegerLiteral(-3)),
+                                Box::new(TermNode::Nop),
+                            ),
+                            Box::new(RelationNode::Nop),
+                        ),
+                        Box::new(ArithmeticNode::Nop),
+                    )),
+                ),
+                context
+                    .context
+                    .i64_type()
+                    .const_int(6, true)
+                    .as_basic_value_enum(),
+            ),
+            (
+                ArithmeticNode::Start(
+                    RelationNode::Start(
+                        TermNode::Start(
+                            FactorNode::Number(NumberNode::IntegerLiteral(3)),
+                            Box::new(TermNode::Nop),
+                        ),
+                        Box::new(RelationNode::Nop),
+                    ),
+                    Box::new(ArithmeticNode::Sub(
+                        RelationNode::Start(
+                            TermNode::Start(
+                                FactorNode::Number(NumberNode::FloatLiteral(3.0)),
+                                Box::new(TermNode::Nop),
+                            ),
+                            Box::new(RelationNode::Nop),
+                        ),
+                        Box::new(ArithmeticNode::Nop),
+                    )),
+                ),
+                context
+                    .context
+                    .f64_type()
+                    .const_float(0.0)
+                    .as_basic_value_enum(),
+            ),
+            (
+                ArithmeticNode::Start(
+                    RelationNode::Start(
+                        TermNode::Start(
+                            FactorNode::Number(NumberNode::FloatLiteral(3.0)),
+                            Box::new(TermNode::Nop),
+                        ),
+                        Box::new(RelationNode::Nop),
+                    ),
+                    Box::new(ArithmeticNode::Sub(
+                        RelationNode::Start(
+                            TermNode::Start(
+                                FactorNode::Number(NumberNode::FloatLiteral(3.0)),
+                                Box::new(TermNode::Nop),
+                            ),
+                            Box::new(RelationNode::Nop),
+                        ),
+                        Box::new(ArithmeticNode::Nop),
+                    )),
+                ),
+                context
+                    .context
+                    .f64_type()
+                    .const_float(0.0)
+                    .as_basic_value_enum(),
+            ),
+        ];
+
+        let failed_tests: Vec<_> = tests
+            .into_iter()
+            .filter_map(|(arithmetic_node, expected_result)| {
+                let formatted_arithmetic_node = format!("{arithmetic_node:?}");
+
+                let result = arithmetic_node.generate_code(&context);
+
+                match result {
+                    Ok(result) => {
+                        if result != expected_result {
+                            Some(format!(
+                                "\t{formatted_arithmetic_node}: {result} != {expected_result}"
+                            ))
+                        } else {
+                            None
+                        }
+                    }
+                    Err(e) => Some(format!(
+                        "\t{formatted_arithmetic_node} encountered an error on code generation!\n{e}"
+                    )),
+                }
+            })
+            .collect();
+
+        if !failed_tests.is_empty() {
+            let formatted = failed_tests.join("\n");
+            panic!("FactorNode Code Generation test(s) failed:\n{formatted}");
+        }
+    }
+}
