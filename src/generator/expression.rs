3be5a12fc103b5670128881621ad330ca9daa08f
John Whiting <thejwhiting@gmail.com>
2024-03-02 19:08:24 -0500

    feat: added tests and NOT code generation support for expression nodes

--- src/generator/expression.rs
+++ src/generator/expression.rs
@@ -23,6 +23,9 @@ pub enum ExpressionNodeCodeGenerationError {
     #[error("COMPILER CODE GENERATION ERROR: Multiple TermNode::Start nodes found in a chain. There is likely a bug in the parser.")]
     MetaDoubleStart,
 
+    #[error("Unable to perform NOT on {0}.")]
+    BitwiseNotUnsupportedType(String),
+
     #[error("Unable to AND {0} with {1}.")]
     BitwiseAndUnsupportedTypes(String, String),
 
@@ -37,11 +40,23 @@ impl<'a> CodeGenerator<'a> for ExpressionNode {
         let (left, right) = match self {
             ExpressionNode::Start(left, right) => {
                 let left = match left {
-                    ExpressionData::Not(_) => {
-                        todo!("NOT needs support still... boolean vs. bitwise not")
+                    ExpressionData::Not(node) => {
+                        let node = node.generate_code(context)?;
+
+                        match node {
+                            BasicValueEnum::IntValue(int_value) => Ok(context
+                                .builder
+                                .build_not(int_value, "tmpnot")?
+                                .as_basic_value_enum()),
+                            x => Err(
+                                ExpressionNodeCodeGenerationError::BitwiseNotUnsupportedType(
+                                    x.error_hint(),
+                                ),
+                            ),
+                        }
                     }
-                    ExpressionData::Nop(node) => node.generate_code(context)?,
-                };
+                    ExpressionData::Nop(node) => Ok(node.generate_code(context)?),
+                }?;
                 Ok((left, right))
             }
             ExpressionNode::And(left, right) | ExpressionNode::Or(left, right) => {
@@ -93,6 +108,197 @@ impl<'a> CodeGenerator<'a> for ExpressionNode {
                 }
             }
             ExpressionNode::Nop => Ok(left),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use inkwell::context::Context;
+
+    use crate::parser::{
+        expression::{ArithmeticNode, FactorNode, RelationNode, TermNode},
+        general::NumberNode,
+    };
+
+    use super::*;
+
+    macro_rules! simple_expression_node {
+        (!$factor_node: expr) => {
+            ExpressionNode::Start(
+                ExpressionData::Not(ArithmeticNode::Start(
+                    RelationNode::Start(
+                        TermNode::Start($factor_node, Box::new(TermNode::Nop)),
+                        Box::new(RelationNode::Nop),
+                    ),
+                    Box::new(ArithmeticNode::Nop),
+                )),
+                Box::new(ExpressionNode::Nop),
+            )
+        };
+        (($factor_node: expr) & ($factor_node_2: expr)) => {
+            ExpressionNode::Start(
+                ExpressionData::Nop(ArithmeticNode::Start(
+                    RelationNode::Start(
+                        TermNode::Start($factor_node, Box::new(TermNode::Nop)),
+                        Box::new(RelationNode::Nop),
+                    ),
+                    Box::new(ArithmeticNode::Nop),
+                )),
+                Box::new(ExpressionNode::And(
+                    ArithmeticNode::Start(
+                        RelationNode::Start(
+                            TermNode::Start($factor_node_2, Box::new(TermNode::Nop)),
+                            Box::new(RelationNode::Nop),
+                        ),
+                        Box::new(ArithmeticNode::Nop),
+                    ),
+                    Box::new(ExpressionNode::Nop),
+                )),
+            )
+        };
+        (($factor_node: expr) | ($factor_node_2: expr)) => {
+            ExpressionNode::Start(
+                ExpressionData::Nop(ArithmeticNode::Start(
+                    RelationNode::Start(
+                        TermNode::Start($factor_node, Box::new(TermNode::Nop)),
+                        Box::new(RelationNode::Nop),
+                    ),
+                    Box::new(ArithmeticNode::Nop),
+                )),
+                Box::new(ExpressionNode::Or(
+                    ArithmeticNode::Start(
+                        RelationNode::Start(
+                            TermNode::Start($factor_node_2, Box::new(TermNode::Nop)),
+                            Box::new(RelationNode::Nop),
+                        ),
+                        Box::new(ArithmeticNode::Nop),
+                    ),
+                    Box::new(ExpressionNode::Nop),
+                )),
+            )
+        };
+        ($factor_node: expr) => {
+            ExpressionNode::Start(
+                ExpressionData::Nop(ArithmeticNode::Start(
+                    RelationNode::Start(
+                        TermNode::Start($factor_node, Box::new(TermNode::Nop)),
+                        Box::new(RelationNode::Nop),
+                    ),
+                    Box::new(ArithmeticNode::Nop),
+                )),
+                Box::new(ExpressionNode::Nop),
+            )
+        };
+    }
+
+    #[test]
+    fn expression_node_generation() {
+        let outside_context = Context::create();
+        let context = CodeGeneratorContext::new(&outside_context);
+        let function_type = context.context.void_type().fn_type(&[], false);
+        let function_value = context.module.add_function("main", function_type, None);
+        let function_entry_block = context.context.append_basic_block(function_value, "entry");
+        context.builder.position_at_end(function_entry_block);
+
+        let tests = vec![
+            (
+                simple_expression_node!(!FactorNode::False),
+                context.context.bool_type().const_int(1, false),
+            ),
+            (
+                simple_expression_node!(!FactorNode::True),
+                context.context.bool_type().const_int(0, false),
+            ),
+            (
+                simple_expression_node!((FactorNode::True) & (FactorNode::True)),
+                context.context.bool_type().const_int(1, false),
+            ),
+            (
+                simple_expression_node!((FactorNode::True) & (FactorNode::False)),
+                context.context.bool_type().const_int(0, false),
+            ),
+            (
+                simple_expression_node!((FactorNode::False) & (FactorNode::True)),
+                context.context.bool_type().const_int(0, false),
+            ),
+            (
+                simple_expression_node!((FactorNode::False) & (FactorNode::False)),
+                context.context.bool_type().const_int(0, false),
+            ),
+            (
+                simple_expression_node!((FactorNode::True) | (FactorNode::True)),
+                context.context.bool_type().const_int(1, false),
+            ),
+            (
+                simple_expression_node!((FactorNode::True) | (FactorNode::False)),
+                context.context.bool_type().const_int(1, false),
+            ),
+            (
+                simple_expression_node!((FactorNode::False) | (FactorNode::True)),
+                context.context.bool_type().const_int(1, false),
+            ),
+            (
+                simple_expression_node!((FactorNode::False) | (FactorNode::False)),
+                context.context.bool_type().const_int(0, false),
+            ),
+            (
+                simple_expression_node!(!FactorNode::Number(NumberNode::IntegerLiteral(1))),
+                context
+                    .context
+                    .i64_type()
+                    .const_int(0xfffffffffffffffe, true),
+            ),
+            (
+                simple_expression_node!(!FactorNode::Number(NumberNode::IntegerLiteral(0))),
+                context
+                    .context
+                    .i64_type()
+                    .const_int(0xffffffffffffffff, true),
+            ),
+            (
+                simple_expression_node!(
+                    (FactorNode::Number(NumberNode::IntegerLiteral(12)))
+                        & (FactorNode::Number(NumberNode::IntegerLiteral(5)))
+                ),
+                context.context.i64_type().const_int(4, true),
+            ),
+            (
+                simple_expression_node!(
+                    (FactorNode::Number(NumberNode::IntegerLiteral(12)))
+                        | (FactorNode::Number(NumberNode::IntegerLiteral(5)))
+                ),
+                context.context.i64_type().const_int(13, true),
+            ),
+        ];
+
+        let failed_tests: Vec<_> = tests
+            .into_iter()
+            .filter_map(|(expression_node, expected_result)| {
+                let formatted_expression_node = format!("{expression_node:?}");
+
+                let result = expression_node.generate_code(&context);
+
+                match result {
+                    Ok(result) => {
+                        if result != expected_result {
+                            Some(format!(
+                                "\t{formatted_expression_node}: {result} != {expected_result}"
+                            ))
+                        } else {
+                            None
+                        }
+                    }
+                    Err(e) => Some(format!(
+                        "\t{formatted_expression_node} encountered an error on code generation!\n{e}"
+                    )),
+                }
+            })
+            .collect();
+
+        if !failed_tests.is_empty() {
+            let formatted = failed_tests.join("\n");
+            panic!("FactorNode Code Generation test(s) failed:\n{formatted}");
         }
     }
 }
